b964b48dc7e0d89da7f77172709725f0525fe09d
maxtian-xuanlan <mxt6232996077@gmail.com>
2026-02-19 05:11:18 -0800

    Formalize module exports

--- src/omni_osint_query/routers/__init__.py
+++ src/omni_osint_query/routers/__init__.py
@@ -1,3 +1,5 @@
-from .health import router as health_router
-from .query_events import router as query_router
-from .query_neighbors import router as neighbors_router
+from omni_osint_query.routers.health import router as health_router
+from omni_osint_query.routers.query_events import router as query_router
+from omni_osint_query.routers.query_neighbors import router as neighbors_router
+
+__all__ = ["health_router", "query_router", "neighbors_router"]
