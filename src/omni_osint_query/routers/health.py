9687da31f85893fcc53514bdd5d303884e4794ed
maxtian-xuanlan <mxt6232996077@gmail.com>
2026-03-31 01:50:35 -0700

    Support debounced embedding update

--- src/omni_osint_query/routers/health.py
+++ src/omni_osint_query/routers/health.py
@@ -9,7 +9,12 @@ class HealthCheck(BaseModel):
     status: str = Field(description="The health status of the service.")
 
 
-@router.get("/health/osint/query", response_model=HealthCheck, operation_id="health_check")
+@router.get(
+    "/health/osint/query",
+    response_model=HealthCheck,
+    operation_id="health_check",
+    include_in_schema=False,
+)
 def health_check():
     ArangoDBClient().db.version()
     RedisClient().client.ping()
